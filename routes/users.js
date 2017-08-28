e7ff046001307e36f112149ee414790e45175ce3
ThomasElkins <elkins.thomas@gmail.com>
2017-08-28 11:25:08 -0700

    User routes and main user page view

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,63 @@
 var express = require('express');
 var router = express.Router();
+// var knex = require ('../db/knex')
 
-/* GET users listing. */
+//Splash page
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users/users');
 });
 
+//Create new user form
+router.get('/new', function(req, res, next){
+  res.send("get request new user page")
+});
+
+//Post new user to database
+router.post('/new', function(req, res, next){
+  res.send("post request to new")
+});
+
+//Delete user from database
+router.post('/:id/delete', function(req, res, next){
+  res.send("delete user")
+});
+
+//Show single user
+router.get('/:id', function(req, res, next){
+  res.send("single user show page")
+});
+
+//Edit user form
+router.get('/:id/edit', function(req, res, next){
+  res.send("edit user form")
+});
+
+//Update user in database
+router.post('/:id/edit', function(req, res, next){
+  res.send("update user in database")
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
 module.exports = router;
