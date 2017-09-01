b0f77bfbaadd7156b38a2a42219ca55f8e6b018d
kylebarker <kyle.barker.24@gmail.com>
2017-09-01 10:09:46 -0700

    integrated pg to heroku

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,8 @@ module.exports = {
     client: 'pg',
     connection: {
       database: 'q2db',
-      host: '127.0.0.1'
+      DATABASE_URL: 'postgres://qetkdkynlnqqme:6c609d71d0419fbb2a7de7f5a1a4f329a6c3b6e738a97f57e4d348b091776114@ec2-204-236-236-188.compute-1.amazonaws.com:5432/d4emghjonvc0a2',
+      host: 'https://powerful-waters-74127.herokuapp.com/'
     },
     seeds: {
       directory: './seeds'
@@ -30,13 +31,10 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      database: 'q2db',
-      host: '127.0.0.1'
-    },
+    connection: 'postgres://qetkdkynlnqqme:6c609d71d0419fbb2a7de7f5a1a4f329a6c3b6e738a97f57e4d348b091776114@ec2-204-236-236-188.compute-1.amazonaws.com:5432/d4emghjonvc0a2',
     seeds: {
       directory: './seeds'
-    }
-  }
+    };
+  };
 
 };
