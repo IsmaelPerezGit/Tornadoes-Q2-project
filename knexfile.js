88c850b7cc6b729ad9d949b30dcd736e882a5762
kylebarker <kyle.barker.24@gmail.com>
2017-09-01 09:31:38 -0700

    personal css changes

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
     client: 'pg',
     connection: {
       database: 'q2db',
-      host: 'https://sheltered-hollows-36302.herokuapp.com/'
+      host: '127.0.0.1'
     },
     seeds: {
       directory: './seeds'
@@ -32,7 +32,7 @@ module.exports = {
     client: 'pg',
     connection: {
       database: 'q2db',
-      host: 'https://sheltered-hollows-36302.herokuapp.com/'
+      host: '127.0.0.1'
     },
     seeds: {
       directory: './seeds'
